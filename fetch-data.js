130fe5cff64f4df1fce4e7f17fcd092fc9847cbb
Finidi47 <finidisaid@gmail.com>
2025-06-17 14:34:36 +0300

    Initialize async function and fetch data

--- fetch-data.js
+++ fetch-data.js
@@ -1,3 +1,24 @@
+// Initialize the Async Function:
 async function fetchUserData() {
-    
+    //Define the API URL:
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    //Select the HTML element where the API data will be displayed:
+    const dataContainer = document.getElementById('api-data');
+
+    // Fetch the data
+    try {
+        const response = await fetch(apiUrl);
+        //convert response to json and store it in users
+        const users = await response.json();
+        console.log(users);
+        console.log(response);
+    } catch (error) {
+        console.log('Failed to load user data.');
+    }
+    //clear content in the data container
+    dataContainer.innerHTML = '';
+
 }
+
+fetchUserData();
